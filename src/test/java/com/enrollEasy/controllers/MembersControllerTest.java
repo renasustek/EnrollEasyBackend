f0dde5038ca7c8e9065d144d3630a8255df6a6a9
renasustek <renasustek@gmail.com>
2025-10-24 00:34:36 +0100

    add unit tests

---
+++ src/test/java/com/enrollEasy/controllers/MembersControllerTest.java
@@ -0,0 +1,38 @@
+package com.enrollEasy.controllers;
+
+import com.enrollEasy.persistance.entites.MemberDao;
+import com.enrollEasy.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = MembersController.class)
+public class MembersControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberService memberService;
+    @Test
+    void getAllMembers() throws Exception {
+        List<MemberDao> memberDaoList  = new ArrayList<>();
+        memberDaoList.add(new MemberDao());
+        given(memberService.getMembers()).willReturn(memberDaoList);
+
+        mockMvc.perform(get("/members"))
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/enrollEasy/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package com.enrollEasy.service;
+
+import com.enrollEasy.persistance.MemberRepo;
+import com.enrollEasy.persistance.entites.MemberDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+class MemberServiceTest {
+
+    @Mock
+    private MemberRepo memberRepo;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    void getMembers(){
+        List<MemberDao> memberDaoList  = new ArrayList<>();
+        memberDaoList.add(new MemberDao());
+
+        given(memberRepo.findAll()).willReturn(memberDaoList);
+
+        List<MemberDao> response = memberService.getMembers();
+
+        assertThat(response).isEqualTo(memberDaoList);
+
+    }
+}
